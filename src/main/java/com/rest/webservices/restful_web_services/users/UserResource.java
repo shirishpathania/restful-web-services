fdc90fb5012de8cb472671192e406229237b6bef
shirishpathania <shirishpathania@gmail.com>
2024-11-18 21:37:00 +1300

    Added versioning Added hal links

--- src/main/java/com/rest/webservices/restful_web_services/users/UserResource.java
+++ src/main/java/com/rest/webservices/restful_web_services/users/UserResource.java
@@ -1,6 +1,8 @@
 package com.rest.webservices.restful_web_services.users;
 
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -8,12 +10,15 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 public class UserResource {
 
     private UserDaoService userDaoService;
 
-    private UserResource(UserDaoService userDaoService) {
+    public UserResource(UserDaoService userDaoService) {
         this.userDaoService = userDaoService;
     }
 
@@ -23,12 +28,15 @@ public class UserResource {
     }
 
     @GetMapping(path = "/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = userDaoService.findUser(id);
         if (user == null) {
             throw new UserNotFoundException("id:" + id);
         }
-        return user;
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+        return entityModel;
     }
 
     @PostMapping(path = "/users")
